4776d073cffcaa3a042929a95bb6290aad27db18
6hislain <ghislainmike80@gmail.com>
2023-09-18 10:33:54 +0200

    new charts

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,105 +1,87 @@
 import { Grid, Card, Header, Statistic } from "semantic-ui-react";
-import {
-  AreaChart,
-  Area,
-  Pie,
-  Cell,
-  PieChart,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-} from "recharts";
 import Layout from "@/layouts/admin";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line, Doughnut } from "react-chartjs-2";
 
-interface CustomLabelProps {
-  cx: number;
-  cy: number;
-  midAngle: number;
-  innerRadius: number;
-  outerRadius: number;
-  percent: number;
-}
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-const data = [
-  {
-    name: "Jan",
-    uv: 40,
-    pv: 24,
-    amt: 24,
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
   },
-  {
-    name: "Feb",
-    uv: 30,
-    pv: 13,
-    amt: 22,
-  },
-  {
-    name: "Mar",
-    uv: 20,
-    pv: 98,
-    amt: 22,
-  },
-  {
-    name: "Apr",
-    uv: 27,
-    pv: 39,
-    amt: 20,
-  },
-  {
-    name: "May",
-    uv: 18,
-    pv: 48,
-    amt: 21,
-  },
-  {
-    name: "Jun",
-    uv: 23,
-    pv: 38,
-    amt: 25,
-  },
-  {
-    name: "Jul",
-    uv: 34,
-    pv: 43,
-    amt: 21,
-  },
-];
+};
 
-const data1 = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 },
-];
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
 
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+const lineData = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => Math.floor(Math.random() * 10)),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => Math.floor(Math.random() * 10)),
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
 
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  percent,
-}: CustomLabelProps) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-  return (
-    <text
-      x={x}
-      y={y}
-      fill="white"
-      textAnchor={x > cx ? "start" : "end"}
-      dominantBaseline="central"
-    >
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  );
+const doughnutData = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
 };
 
 const Dashboard = () => {
@@ -152,68 +134,11 @@ const Dashboard = () => {
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
-          <Grid.Column width={8}>
-            <ResponsiveContainer width="100%" height={500}>
-              <AreaChart
-                height={400}
-                data={data}
-                margin={{
-                  top: 10,
-                  right: 30,
-                  left: 0,
-                  bottom: 0,
-                }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Area
-                  type="monotone"
-                  dataKey="uv"
-                  stackId="1"
-                  stroke="#8884d8"
-                  fill="#8884d8"
-                />
-                <Area
-                  type="monotone"
-                  dataKey="pv"
-                  stackId="1"
-                  stroke="#82ca9d"
-                  fill="#82ca9d"
-                />
-                <Area
-                  type="monotone"
-                  dataKey="amt"
-                  stackId="1"
-                  stroke="#ffc658"
-                  fill="#ffc658"
-                />
-              </AreaChart>
-            </ResponsiveContainer>
+          <Grid.Column width={11}>
+            <Line options={options} data={lineData} />
           </Grid.Column>
-          <Grid.Column width={8}>
-            <ResponsiveContainer width="100%" height="100%">
-              <PieChart width={400} height={400}>
-                <Pie
-                  data={data1}
-                  cx="50%"
-                  cy="50%"
-                  labelLine={false}
-                  label={renderCustomizedLabel}
-                  outerRadius={160}
-                  fill="#8884d8"
-                  dataKey="value"
-                >
-                  {data1.map((_entry, index) => (
-                    <Cell
-                      key={`cell-${index}`}
-                      fill={COLORS[index % COLORS.length]}
-                    />
-                  ))}
-                </Pie>
-              </PieChart>
-            </ResponsiveContainer>
+          <Grid.Column width={5}>
+            <Doughnut data={doughnutData} />
           </Grid.Column>
         </Grid.Row>
       </Grid>
