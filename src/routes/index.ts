ef77f6b4337ca5c4e9c69aecbeae66fdaf547c1e
6hislain <ghislainmike80@gmail.com>
2024-01-14 18:39:58 +0200

    .

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -66,7 +66,40 @@ router.get("/report", async (req: Request, res: Response) => {
   const admissions = await AdmissionModel.aggregate([
     { $group: { _id: "$finalDiagnosis", count: { $sum: 1 } } },
   ]);
-  res.json({ admissions });
+  const monthly = await AdmissionModel.aggregate([
+    {
+      $group: {
+        _id: { month: { $month: "$admissionDate" } },
+        count: { $sum: 1 },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        month: {
+          $switch: {
+            branches: [
+              { case: { $eq: ["$_id.month", 1] }, then: "January" },
+              { case: { $eq: ["$_id.month", 2] }, then: "February" },
+              { case: { $eq: ["$_id.month", 3] }, then: "March" },
+              { case: { $eq: ["$_id.month", 4] }, then: "April" },
+              { case: { $eq: ["$_id.month", 5] }, then: "May" },
+              { case: { $eq: ["$_id.month", 6] }, then: "June" },
+              { case: { $eq: ["$_id.month", 7] }, then: "July" },
+              { case: { $eq: ["$_id.month", 8] }, then: "August" },
+              { case: { $eq: ["$_id.month", 9] }, then: "September" },
+              { case: { $eq: ["$_id.month", 10] }, then: "October" },
+              { case: { $eq: ["$_id.month", 11] }, then: "November" },
+              { case: { $eq: ["$_id.month", 12] }, then: "December" },
+            ],
+            default: "Unknown",
+          },
+        },
+        count: 1,
+      },
+    },
+  ]);
+  res.json({ admissions, monthly });
 });
 
 /**
